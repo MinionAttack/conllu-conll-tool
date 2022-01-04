0ad9ae618d4a8800937914ad7cc944eb0c08debf
MinionAttack <10078549+MinionAttack@users.noreply.github.com>
2022-01-04 13:21:43 +0100

    Preserve commented lines

--- modules/remove_pos.py
+++ modules/remove_pos.py
@@ -21,12 +21,15 @@ def remove_pos_column(files: List[Path], output_path: Path) -> None:
         output_file = Path(output_path).joinpath(filename)
         with open(file, 'rt', encoding='UTF-8') as pos_file, open(output_file, 'wt', encoding='UTF-8') as no_pos_file:
             for line in pos_file:
-                if line != "\n":
-                    tuples = line.split("\t")
-                    if len(tuples) == 10 and tuples[0] != '#' and '.' not in tuples[0] and '-' not in tuples[0]:
-                        tuples[3] = '_'
-                        no_pos_file.write('\t'.join(tuples))
+                if not line.startswith("# sent_id =") and not line.startswith("# text ="):
+                    if line != "\n":
+                        tuples = line.split("\t")
+                        if len(tuples) == 10 and tuples[0] != '#' and '.' not in tuples[0] and '-' not in tuples[0]:
+                            tuples[3] = '_'
+                            no_pos_file.write('\t'.join(tuples))
+                        else:
+                            no_pos_file.write(line)
                     else:
-                        no_pos_file.write(line)
+                        no_pos_file.write("\n")
                 else:
-                    no_pos_file.write("\n")
+                    no_pos_file.write(line)
